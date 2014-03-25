42215f99e0bec395497a4887b734f636d06659d4
Alex Ray <aray@google.com>
2014-03-24 20:01:56 -0700

    mdnsd: build static executable
    mdnsd is used in the fastbootd image, which isn't loaded with dynamic libraries.
    
    Change-Id: I082638163551d9042ec37b51d856463d801074f1

--- Android.mk
+++ Android.mk
@@ -24,9 +24,8 @@ LOCAL_C_INCLUDES := external/mdnsresponder/mDNSPosix \
                     external/mdnsresponder/mDNSShared
 
 LOCAL_CFLAGS := -O2 -g -W -Wall -D__ANDROID__ -D_GNU_SOURCE -DHAVE_IPV6 -DNOT_HAVE_SA_LEN -DUSES_NETLINK -DTARGET_OS_LINUX -fno-strict-aliasing -DHAVE_LINUX -DMDNS_DEBUGMSGS=0 -DMDNS_UDS_SERVERPATH=\"/dev/socket/mdnsd\" -DMDNS_USERNAME=\"mdnsr\" -DPLATFORM_NO_RLIMIT
-LOCAL_SYSTEM_SHARED_LIBRARIES := libc
-LOCAL_SHARED_LIBRARIES := libcutils liblog
-
+LOCAL_STATIC_LIBRARIES := libc libcutils liblog
+LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
 
 ##########################
