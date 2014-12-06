95c5a47184eacf12ea92c47846b5c461fd8b8360
Trevor Drake <trevordrake.gerrit@gmail.com>
2014-12-06 13:57:14 +0000

    Clean up Android.mk
    mdnsd : Moved declaration the end of the file so the target can make
    	use of commonFlags and commonLibs
    
    libmdnssd : Added LOCAL_EXPORT_C_INCLUDE_DIRS to static and shared
    	    library targets
    
    dnssd : Removed redundant LOCAL_C_INCLUDES from dnssd as target now uses
    	libmdnssd's LOCAL_EXPORT_C_INCLUDE_DIRS instead.
    
    dnssd : Use commonFlags for common LOCAL_CFLAGS
    
    dnssd : Use commonLibs for common LOCAL_SHARED_LIBRARIES
    
    mdnsd : Use commonFlags for common LOCAL_CFLAGS
    
    mdnsd : Use commonLibs for common LOCAL_STATIC_LIBRARIES
    
    Tested using clean out directory with aosp_arm-eng and aosp_x86_64-eng
    
    make -j16 gptedit fastbootd libvendortrigger.default dnssd mdnsd netd
    
    Change-Id: Ie6295fc625149392fd19b487e70d8ae0819f52ac

--- Android.mk
+++ Android.mk
@@ -1,5 +1,53 @@
 LOCAL_PATH := $(call my-dir)
 
+##########################
+
+commonSources := \
+    mDNSShared/dnssd_clientlib.c  \
+    mDNSShared/dnssd_clientstub.c \
+    mDNSShared/dnssd_ipc.c
+
+commonLibs := libcutils liblog
+
+commonFlags := -O2 -g -W -Wall -D__ANDROID__ -D_GNU_SOURCE -DHAVE_IPV6 \
+    -DNOT_HAVE_SA_LEN -DUSES_NETLINK -DTARGET_OS_LINUX -fno-strict-aliasing \
+    -DHAVE_LINUX -DMDNS_UDS_SERVERPATH=\"/dev/socket/mdnsd\" -DMDNS_DEBUGMSGS=0
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(commonSources)
+LOCAL_MODULE := libmdnssd
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS := $(commonFlags)
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc
+LOCAL_SHARED_LIBRARIES := $(commonLibs)
+LOCAL_EXPORT_C_INCLUDE_DIRS := external/mdnsresponder/mDNSShared
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(commonSources)
+LOCAL_MODULE := libmdnssd
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS := $(commonFlags)
+LOCAL_STATIC_LIBRARIES := $(commonLibs)
+LOCAL_EXPORT_C_INCLUDE_DIRS := external/mdnsresponder/mDNSShared
+include $(BUILD_STATIC_LIBRARY)
+
+############################
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES :=  Clients/dns-sd.c \
+                    Clients/ClientCommon.c
+
+LOCAL_MODULE := dnssd
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_CFLAGS := $(commonFlags) -DMDNS_USERNAME=\"mdnsr\" -DPLATFORM_NO_RLIMIT
+
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc
+LOCAL_SHARED_LIBRARIES := libmdnssd $(commonLibs)
+
+include $(BUILD_EXECUTABLE)
+
 #########################
 
 include $(CLEAR_VARS)
@@ -23,55 +71,7 @@ LOCAL_C_INCLUDES := external/mdnsresponder/mDNSPosix \
                     external/mdnsresponder/mDNSCore  \
                     external/mdnsresponder/mDNSShared
 
-LOCAL_CFLAGS := -O2 -g -W -Wall -D__ANDROID__ -D_GNU_SOURCE -DHAVE_IPV6 -DNOT_HAVE_SA_LEN -DUSES_NETLINK -DTARGET_OS_LINUX -fno-strict-aliasing -DHAVE_LINUX -DMDNS_DEBUGMSGS=0 -DMDNS_UDS_SERVERPATH=\"/dev/socket/mdnsd\" -DMDNS_USERNAME=\"mdnsr\" -DPLATFORM_NO_RLIMIT
-LOCAL_STATIC_LIBRARIES := libc libcutils liblog
+LOCAL_CFLAGS := $(commonFlags) -DMDNS_USERNAME=\"mdnsr\" -DPLATFORM_NO_RLIMIT
+LOCAL_STATIC_LIBRARIES := libc $(commonLibs)
 LOCAL_FORCE_STATIC_EXECUTABLE := true
-include $(BUILD_EXECUTABLE)
-
-##########################
-
-commonSources := \
-    mDNSShared/dnssd_clientlib.c  \
-    mDNSShared/dnssd_clientstub.c \
-    mDNSShared/dnssd_ipc.c
-
-commonLibs := libcutils liblog
-
-commonFlags := -O2 -g -W -Wall -D__ANDROID__ -D_GNU_SOURCE -DHAVE_IPV6 \
-    -DNOT_HAVE_SA_LEN -DUSES_NETLINK -DTARGET_OS_LINUX -fno-strict-aliasing \
-    -DHAVE_LINUX -DMDNS_UDS_SERVERPATH=\"/dev/socket/mdnsd\" -DMDNS_DEBUGMSGS=0
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := $(commonSources)
-LOCAL_MODULE := libmdnssd
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS := $(commonFlags)
-LOCAL_SYSTEM_SHARED_LIBRARIES := libc
-LOCAL_SHARED_LIBRARIES := $(commonLibs)
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := $(commonSources)
-LOCAL_MODULE := libmdnssd
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS := $(commonFlags)
-LOCAL_STATIC_LIBRARIES := $(commonLibs)
-include $(BUILD_STATIC_LIBRARY)
-
-############################
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES :=  Clients/dns-sd.c \
-                    Clients/ClientCommon.c
-
-LOCAL_MODULE := dnssd
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_C_INCLUDES := external/mdnsresponder/mDNSShared
-
-LOCAL_CFLAGS := -O2 -g -W -Wall -D__ANDROID__ -D_GNU_SOURCE -DHAVE_IPV6 -DNOT_HAVE_SA_LEN -DUSES_NETLINK -DTARGET_OS_LINUX -fno-strict-aliasing -DHAVE_LINUX -DMDNS_UDS_SERVERPATH=\"/dev/socket/mdnsd\" -DMDNS_DEBUGMSGS=0
-
-LOCAL_SYSTEM_SHARED_LIBRARIES := libc
-LOCAL_SHARED_LIBRARIES := libmdnssd libcutils liblog
-
 include $(BUILD_EXECUTABLE)
